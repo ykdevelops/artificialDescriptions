4bfc8e326dbc6fcad990b1283492f5da2b024426
ykdevelops <ykdevelops@gmail.com>
2023-07-13 21:01:16 -0400

    feat: :zap: Created mechanism for all the elements in the array to be displayed

--- src/app/Components/artworkTemplate.tsx
+++ src/app/Components/artworkTemplate.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import galleryStyles from "../gallery.module.css";
-import galleryData from "../assets/galleryDetails.json";
 
-interface GalleryItem {
+interface ArtworkTemplateProps {
   imageUrl: string;
   musicLink: string;
   description: string;
 }
 
-interface GalleryData {
-  gallery: GalleryItem[];
-}
-
-export default function ArtworkTemplate() {
+const ArtworkTemplate: React.FC<ArtworkTemplateProps> = ({
+  imageUrl,
+  musicLink,
+  description,
+}) => {
   const createMarkup = (htmlString: string) => {
     return { __html: htmlString };
   };
@@ -22,20 +21,11 @@ export default function ArtworkTemplate() {
     <div className={galleryStyles.artSection}>
       <div className={galleryStyles.topRow}>
         <div className={galleryStyles.leftHalf}>
-          <Image
-            src={galleryData.gallery[0].imageUrl}
-            alt="Artwork"
-            width={400}
-            height={400}
-          />
+          <Image src={imageUrl} alt="Artwork" width={400} height={400} />
         </div>
 
         <div className={galleryStyles.rightHalf}>
-          <div
-            dangerouslySetInnerHTML={createMarkup(
-              galleryData.gallery[0].description
-            )}
-          />
+          <div dangerouslySetInnerHTML={createMarkup(description)} />
         </div>
       </div>
       <div className={galleryStyles.soundcloud}>
@@ -45,9 +35,11 @@ export default function ArtworkTemplate() {
           scrolling="no"
           frameBorder="no"
           allow="autoplay"
-          src={galleryData.gallery[0].musicLink}
+          src={musicLink}
         ></iframe>
       </div>
     </div>
   );
-}
+};
+
+export default ArtworkTemplate;
--- src/app/Components/gallery.tsx
+++ src/app/Components/gallery.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ArtworkTemplate from "./artworkTemplate";
+import galleryData from "../assets/galleryDetails.json";
+
 export default function Gallery() {
   return (
     <div>
-      <ArtworkTemplate />
+      {galleryData.gallery.map((item, index) => (
+        <ArtworkTemplate
+          key={index}
+          imageUrl={item.imageUrl}
+          musicLink={item.musicLink}
+          description={item.description}
+        />
+      ))}
     </div>
   );
 }
