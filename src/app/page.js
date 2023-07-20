967ccc5c2220b05993982ed6724dfbf69135d36d
ykdevelops <ykdevelops@gmail.com>
2023-07-19 22:36:22 -0400

    feat: :art: Created intro component

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Gallery from "./components/gallery"
+import Intro from "./components/intro"
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Intro />
       <Gallery />
-      <div className={styles.description}>
-        <p>
-          Welcome to our AI Art Expo, a fascinating blend of AI-generated
-          artwork paired with AI-rendered descriptions.
-        </p>
-        <p>
-          Our exhibition aims to enrich your interaction with AI, by offering
-          you an insight into the language utilized in the artworks. As you
-          explore, you&apos;ll uncover patterns, discover new artistic styles,
-          and gather abundant inspiration.
-        </p>
-        <p>
-          Leveraging the powers of Midjourney and DALL-E, we&apos;ve created a
-          collection inspired by the auditory waves from the SoundCloud page
-          &quot;djlo2us&quot;. Get ready to immerse yourself in a unique journey
-          where art and AI coalesce in the most intriguing ways.
-        </p>
-      </div>
     </main>
   );
 }
